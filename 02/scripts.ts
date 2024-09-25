8c37b39b09e5bc85beb17eb285e66839754178ab
Rodrigo <rtrancozo@gmail.com>
2024-09-25 17:51:43 -0300

    commit das resoluções dos exercícios

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,35 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string
+    rua: string
+    bairro: string
+    cidade: string
+    estado: string
+
+}
+
+// type NovoCarrinho = {
+//     item: Item,
+//     qtd: number,
+//     desconto: number,
+//     frete: number,
+//     endereco: {
+//         cep:string
+//         rua:string
+//         bairro:string
+//         cidade:string
+//         estado:string
+//     },
+//     tipoTransacao: Lowercase<'credito' | 'Debito'>,
+//     cartao: Cartao
+// }
+
+type NovoCarrinho = Omit<Carrinho, 'tipoTransacao'> & {
+    endereco: Endereco,
+    tipoTransacao: Lowercase<'credito' | 'Debito'>
+}
+
+
